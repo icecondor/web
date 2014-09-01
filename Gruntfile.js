60e5c86c08f4b39fb61dd2ad3aab7d7e7b23ea80
Don Park <donp@donp.org>
2014-09-01 16:32:29 -0700

    a node/ruby hybrid, all for rust

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,7 +1,14 @@
 module.exports = function(grunt) {
   grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json')
+    pkg: grunt.file.readJSON('package.json'),
+    slim: {
+      dev: {
+        options: { pretty: '' },
+        files: [{ src:'views/*', dest: 'html/index.html'}]
+      }
+    }
   });
   grunt.loadNpmTasks('grunt-slim');
+  grunt.registerTask('default', ['slim']);
 };
 
