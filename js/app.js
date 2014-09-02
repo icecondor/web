7abe531565f288325e52b9e12b7ba82f0b49c614
Don Park <donp@donp.org>
2014-09-02 15:12:22 -0700

    localStorage and apikey

--- js/app.js
+++ js/app.js
@@ -1,3 +1,26 @@
 function setup() {
+  console.log("existing apikey is "+getKey())
+  apiKeyCheck()
   iceCondor.setup()
+}
+
+function apiKeyCheck() {
+  var key = getQueryParameterByName('apikey')
+  if(key) {
+    console.log("setting api key "+key)
+    setKey(key)
+  }
+}
+
+function getQueryParameterByName(name) {
+    var match = RegExp('[?&]' + name + '=([^&]*)').exec(window.location.search);
+    return match && decodeURIComponent(match[1].replace(/\+/g, ' '));
+}
+
+function getKey() {
+  return localStorage.getItem("apikey")
+}
+
+function setKey(key) {
+  return localStorage.setItem("apikey", key)
 }
