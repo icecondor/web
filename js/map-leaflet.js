7945c997b0cc84e8ff395c2387ff157622af230f
Don Park <donp@donp.org>
2014-09-10 09:43:24 -0700

    US centric

--- js/map-leaflet.js
+++ js/map-leaflet.js
@@ -1,7 +1,8 @@
   this.map_leaflet = {};
 
   map_leaflet.setup = function(center, zoom){
-    map_leaflet.map = L.map('map', {drawControl: true, zoomControl: false}).setView(this.pointToLatLng(center), zoom);
+    map_leaflet.map = L.map('map', {drawControl: true, zoomControl: false}).
+                        setView(this.pointToLatLng(center), zoom);
     var zoom = L.control.zoom({position: 'topright'});
     map_leaflet.map.addControl(zoom);
     map_leaflet.remove_draw();
