58e3b505102973fb9ec7e0a802cad3ecff6c56bf
Don Park <donp@donp.org>
2014-09-25 09:30:36 -0700

    js style

--- js/map-leaflet.js
+++ js/map-leaflet.js
@@ -1,11 +1,12 @@
-  this.map_leaflet = {};
+var map_leaflet = function() {
+  var api = {}
 
-  map_leaflet.setup = function(center, zoom){
-    map_leaflet.map = L.map('map', {drawControl: true, zoomControl: false}).
+  api.setup = function(center, zoom){
+    api.map = L.map('map', {drawControl: true, zoomControl: false}).
                         setView(this.pointToLatLng(center), zoom);
     var zoom = L.control.zoom({position: 'topright'});
-    map_leaflet.map.addControl(zoom);
-    map_leaflet.remove_draw();
+    api.map.addControl(zoom);
+    api.remove_draw();
 
     var osmUrl='//{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
     var osmAttrib='Map data © OpenStreetMap contributors';
@@ -15,39 +16,42 @@
     return map
   }
 
-  map_leaflet.add_draw = function(){
-    map_leaflet.map.drawControl.addTo(map_leaflet.map)
+  api.add_draw = function(){
+    api.map.drawControl.addTo(api.map)
   }
 
-  map_leaflet.remove_draw = function(){
-    map_leaflet.map.drawControl.removeFrom(map_leaflet.map)
+  api.remove_draw = function(){
+    api.map.drawControl.removeFrom(api.map)
   }
 
-  map_leaflet.setCenter = function(center){
+  api.setCenter = function(center){
     this.map.panTo(this.pointToLatLng(center))
   }
 
-  map_leaflet.pointToLatLng = function(point){
+  api.pointToLatLng = function(point){
     return L.latLng(point.coordinates[1], point.coordinates[0])
   }
 
-  map_leaflet.latLngToPoint = function(latlng){
+  api.latLngToPoint = function(latlng){
     return {coordinates: [latlng.lat, latlng.lng]}
   }
 
-  map_leaflet.makeIcon = function(url, w, h){
+  api.makeIcon = function(url, w, h){
     return L.icon({iconUrl: url, iconSize: [w,h]})
   }
 
-  map_leaflet.makeMarker = function(point, title){
+  api.makeMarker = function(point, title){
     var marker = L.marker(this.pointToLatLng(point))
     marker.addTo(this.map)
     return marker
   }
 
-  map_leaflet.makeLine = function(points){
+  api.makeLine = function(points){
     var latlngs = points.map(function(p){this.pointToLatLng(p)})
     var line = L.polyline(latlngs)
-    map_leaflet.map.addLayer(line)
+    api.map.addLayer(line)
     return line
   }
+
+  return api
+}()
