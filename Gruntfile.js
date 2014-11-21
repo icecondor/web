b88338b4ed4087618abbc3a5484435300f0b1029
Don Park <donp@donp.org>
2014-11-21 11:13:08 -0800

    Developer page

--- Gruntfile.js
+++ Gruntfile.js
@@ -15,6 +15,7 @@ module.exports = function(grunt) {
           "build/billing.html": "views/billing.jade",
           "build/p/contact-us.html": "views/page/contact-us.jade",
           "build/p/terms-of-service.html": "views/page/terms-of-service.jade",
+          "build/p/developer.html": "views/page/developer.jade",
           "build/b/index.html": "views/blog/index.jade",
           "build/b/2014/11/21/why-icecondor.html": "views/blog/2014/11/21/why-icecondor.jade",
         }
