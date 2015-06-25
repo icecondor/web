4134b82193f35d6d2f0be0debe0d32f549d05b84
Don Park <donp@donp.org>
2015-06-25 09:05:24 -0700

    too many mind. or params.

--- js/ui/map.js
+++ js/ui/map.js
@@ -35,7 +35,7 @@ function day_selected(evt, layercache){
   stop.addDays(1)
   map.removeTracks()
   $('#last_point').html('')
-  startFollow(params.username, day, stop, 1000, 'oldest', layercache)
+  startFollow(params.username, day, stop, 1000, 'oldest', false, layercache)
 }
 
 function month_day_select_setup(){
