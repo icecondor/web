4542dd3d711b5957c02487ba69b842e3680e5794
dpdp <donp@donp.org>
2021-07-28 11:16:42 -0700

    debug slip

--- js/icecondor.js
+++ js/icecondor.js
@@ -60,10 +60,10 @@ var iceCondor = function() {
         console.log('connect ignored. already connected.')
         resolve()
       } else {
-        host = host || '/localhost:9360/sockjs'
+        host = host || '/sockjs'
         // websocket
-        sock = new WebSocket("ws://localhost:2040");
-        //sock = new SockJS(host);
+        //sock = new WebSocket("ws://localhost:2040");
+        sock = new SockJS(host);
         sock.onmessage = message
         sock.onerror = error
         sock.onopen = function(){
