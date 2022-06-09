7b785ddd2a3c954e131711ecde8465044f1d0873
dp <donp@donp.org>
2022-06-08 17:10:36 -0700

    i had to be unicode tricky

---
+++ js/µtemplate.js
@@ -0,0 +1,17 @@
+var uStache = {}
+uStache.compile = function(str) {
+    var err = "";
+    try {
+      var strFunc =
+      "with(locals){return ['" +
+      str.replace(/[\r\t\n]/g, " ")
+         .split("'").join("\\'")
+         .split("\t").join("'")
+         .replace(/{{(.+?)}}/g, "',$1,'")
+         + "'].join('')}";
+      var func = new Function("locals", strFunc);
+      return func;
+    } catch (e) { err = e.message; }
+    return "< # ERROR: " + err + " # >";
+}
+
